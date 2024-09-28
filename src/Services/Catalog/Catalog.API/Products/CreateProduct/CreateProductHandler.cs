38846f121e30851f589e891569019c12ba99d027
Furkan Gundogan <furkangundogan14@outlook.com>
2024-09-28 11:31:35 +0300

    docker compose added for postgres. Basit catalog api features added with vertical slice arch

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,16 +1,15 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-
-namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct
 {
-
 
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
         :ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+
+    ///  IDocumentSession is abstraction of db operations
+
+    internal class CreateProductCommandHandler (IDocumentSession session)
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
@@ -28,9 +27,10 @@ namespace Catalog.API.Products.CreateProduct
             };
 
             // save to db
-
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
             // return result
-            return new CreateProductResult(Guid.NewGuid());
+            return new CreateProductResult(product.Id);
 
 
             
