80d9ede08918719d9cc54f102d4e89b98b760f16
Furkan Gündoğan <furkangundogan14@outlook.com>
2025-01-15 19:17:58 +0300

    DDD Rich-domain model customer customized for Order.Domain Layer

--- src/Services/Ordering/Ordering.Domain/Models/Customer.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -4,4 +4,20 @@ public class Customer : Entity<CustomerId>
 {
     public string Name { get; private set; } = default!;
     public string Email { get; private set; } = default!;
+
+    public static Customer Create(CustomerId id, string name, string email)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name, nameof(id));
+        ArgumentException.ThrowIfNullOrWhiteSpace(email, nameof(email));
+
+        var customer = new Customer
+        {
+            Id = id,
+            Name = name,
+            Email = email
+        };
+
+        return customer;
+
+    }
 }
--- src/Services/Ordering/Ordering.Domain/Models/Product.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -5,4 +5,6 @@ public class Product : Entity<ProductId>
     public string Name { get; private set; } = default!;
     public decimal Price { get; private set; } = default!;
 
+
+
 }
