36e38fa4aa6a8e3fcfbac84bd7de7e2f0b3818f7
Furkan Gündoğan <furkangundogan14@outlook.com>
2025-03-09 13:50:55 +0300

    Ordering API DeleteOrder and Command added

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,22 @@
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+using Ordering.Application.Dtos;
+
+
+namespace Ordering.Application.Orders.Commands.DeleteOrder;
+
+
+public record DeleteOrderCommand(Guid OrderId) : ICommand <DeleteOrderResult>;
+
+public record DeleteOrderResult(bool IsSuccess);
+
+
+public class DeleteOrderCommandValidator : AbstractValidator<DeleteOrderCommand>
+{
+    public DeleteOrderCommandValidator()
+    {
+        RuleFor(x => x.OrderId).NotEmpty().WithMessage("OrderId is required");
+
+        
+    }
+}
