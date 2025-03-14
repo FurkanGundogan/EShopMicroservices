83139cd4805440b89df993246418d33edc1440d3
Furkan Gündoğan <furkangundogan14@outlook.com>
2025-03-14 23:53:34 +0300

    Order GetOrdersByName queries added

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
@@ -0,0 +1,27 @@
+﻿using Ordering.Application.Extensions;
+using System.Collections.Generic;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByName;
+
+public class GetOrdersByNameHandler(IApplicationDbContext dbContext)
+    : IQueryHandler<GetOrdersByNameQuery, GetOrdersByNameResult>
+{
+    public async Task<GetOrdersByNameResult> Handle(GetOrdersByNameQuery query, CancellationToken cancellationToken)
+    {
+        /// get order by name using dbContext
+        var orders = await dbContext.Orders
+            .Include(o => o.OrderItems)
+            .AsNoTracking()
+            .Where(x => x.OrderName.Value.Contains(query.Name))
+            .OrderBy(x => x.OrderName)
+            .ToListAsync();
+
+        var orderDtos = orders.ToOrderDtoList();
+
+        /// return result
+        return new GetOrdersByNameResult(orderDtos);
+       
+       
+    }
+
+}
