d9948ac76bb066b3e49d7afd7416eb3c5a14ade6
Furkan Gündoğan <furkangundogan14@outlook.com>
2025-03-14 21:12:11 +0300

    OrderCreatedEventHandler and OrderUpdatedEventHandler added

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/OrderCreatedEventHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace Ordering.Application.Orders.EventHandlers;
+
+public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger) : INotificationHandler<OrderCreatedEvent>
+{
+    public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        /// this will be triggered after event published
+        /// we will modify here for to publish integration events
+        logger.LogInformation("Domain Event handled: {DomainEvent}",notification.GetType().Name);
+        return Task.CompletedTask;
+    }
+}
