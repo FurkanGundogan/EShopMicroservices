69801eb02ae1cc84de1f3af830a377ab0fa2789e
Furkan Gündoğan <furkangundogan14@outlook.com>
2025-03-16 10:30:42 +0300

    GetOdersByCustomer handler added to Ordering.Application

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerHandler.cs
@@ -0,0 +1,27 @@
+﻿using Ordering.Application.Extensions;
+using System.Collections.Generic;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+
+public class GetOrdersByCustomerHandler(IApplicationDbContext dbContext)
+    : IQueryHandler<GetOrdersByCustomerQuery, GetOrdersByCustomerResult>
+{
+    public async Task<GetOrdersByCustomerResult> Handle(GetOrdersByCustomerQuery query, CancellationToken cancellationToken)
+    {
+        /// get order by name using dbContext
+        var orders = await dbContext.Orders
+            .Include(o => o.OrderItems)
+            .AsNoTracking()
+            .Where(x => x.CustomerId == CustomerId.Of(query.CustomerId))
+            .OrderBy(x => x.OrderName.Value)
+            .ToListAsync();
+
+        var orderDtos = orders.ToOrderDtoList();
+
+        /// return result
+        return new GetOrdersByCustomerResult(orderDtos);
+       
+       
+    }
+
+}
