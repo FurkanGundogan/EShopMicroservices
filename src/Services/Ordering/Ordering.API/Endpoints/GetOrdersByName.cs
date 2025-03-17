9bddf01b3e3c802464409920009bb019201e382d
Furkan Gündoğan <furkangundogan14@outlook.com>
2025-03-17 18:11:26 +0300

    OrderingAPI endpoint classes created.

---
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.API.Endpoints
+{
+
+    //- Accepts a name parameter.
+    //- Constructs a GetOrdersByNameQuery.
+    //- Retrieves and returns matching orders.
+    public class GetOrdersByName
+    {
+    }
+}
