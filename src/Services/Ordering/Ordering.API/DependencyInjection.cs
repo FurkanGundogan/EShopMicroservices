1a28c73e41009e61c3ae94a7fcaf2d40c0fb6a2f
Furkan Gündoğan <furkangundogan14@outlook.com>
2025-03-22 12:27:35 +0300

    HealthChecks added

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,14 +1,17 @@
 ﻿using BuildingBlocks.Exceptions.Handler;
 using Carter;
+using HealthChecks.UI.Client;
 
 namespace Ordering.API;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddCarter();
         services.AddExceptionHandler<CustomExceptionHandler>();
+        services.AddHealthChecks()
+            .AddSqlServer(configuration.GetConnectionString("Database")!);
         return services;
     }
 
@@ -16,6 +19,11 @@ public static class DependencyInjection
     {
         app.MapCarter();
         app.UseExceptionHandler(opt => { });
+        app.UseHealthChecks("/health",
+            new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
         return app;
     }
 }
