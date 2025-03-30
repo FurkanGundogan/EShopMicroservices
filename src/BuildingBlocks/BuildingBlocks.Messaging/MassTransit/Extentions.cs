9e7f64b4da39cfea0ce2f2d8f8a6de622f603ca2
Furkan Gündoğan <furkangundogan14@outlook.com>
2025-03-30 09:22:19 +0300

    MassTransit configurations added with new buildingblock project

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extentions.cs
@@ -0,0 +1,40 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BuildingBlocks.Messaging.MassTransit;
+
+public static class Extentions
+{
+
+    public static IServiceCollection AddMessageBroker
+        (this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null)
+    {
+        // implement rabbitmq conf
+        services.AddMassTransit(config =>
+        {
+
+            config.SetKebabCaseEndpointNameFormatter();
+
+            if (assembly != null) config.AddConsumers(assembly);
+            // important for consuming from basket and ordering 
+            // for publisher it is null
+
+            config.UsingRabbitMq((context, configurator) =>
+            {
+                configurator.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
+                {
+                    host.Username(configuration["MessageBroker:UserName"]);
+                    host.Password(configuration["MessageBroker:Password"]);
+                });
+                configurator.ConfigureEndpoints(context);
+                // auto configuration endpoints for consumers
+            });
+
+
+        });
+        return services;
+    }
+
+}
